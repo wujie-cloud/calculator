0fbd377482fd1170daa34843d49686e6ac546e92
FeiYehua <31472675+feiyehua@users.noreply.github.com>
2024-12-17 21:20:09 +0800

    edit vcxproj

--- src/DrawTime.cpp
+++ src/DrawTime.cpp
@@ -14,6 +14,6 @@ void drawtime()
     SYSTEMTIME ti;
     GetLocalTime(&ti);
     TCHAR time[256] = {0};
-    swprintf_s(time, _T("%u/%u/%u %02d:%02d:%02d"), ti.wYear, ti.wMonth, ti.wDay, ti.wHour, ti.wMinute, ti.wSecond);
+    swprintf_s(time, _T("%u/%u/%u %02d:%02d:%02d"), ti.wYear, ti.wMonth, ti.wDay-8, ti.wHour, ti.wMinute, ti.wSecond);
     outtextxy(40, 200, time);
 }
